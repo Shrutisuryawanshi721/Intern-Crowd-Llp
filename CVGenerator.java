9941bf98b5b625c16f184a9941e435aec9cc1715
Shrutisuryawanshi721 <86583171+Shrutisuryawanshi721@users.noreply.github.com>
2023-04-03 16:29:41 +0530

    Add files via upload

---
+++ CVGenerator.java
@@ -0,0 +1,17 @@
+
+package cv.generator;
+
+/**
+ *
+ * @author Hyrex
+ */
+public class CVGenerator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
